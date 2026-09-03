01f84ab9753fcb86e38c8ed3b98d67e251c9247a
Thavaseelanvenzo <thavaseelanr@venzotechnologies.com>
2026-09-03 12:38:09 +0530

    test auto deployment

--- pg_backup.js
+++ pg_backup.js
@@ -259,6 +259,7 @@ function loadConfig(log, { dumpOnly = false } = {}) {
     );
   }
 
+  console.log("testing auto deployment flag");
   
   const port = Number(config.DB_PORT);
   if (!Number.isInteger(port) || port < 1 || port > 65535) {
